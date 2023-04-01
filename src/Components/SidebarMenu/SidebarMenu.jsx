bfa216297851130d612ebf84e1c7a429a6524249
hassanabbasi2213 <hassan.ababsi@propsure.com.pk>
2023-04-01 15:14:19 +0500

    changes in navbar design

--- src/Components/SidebarMenu/SidebarMenu.jsx
+++ src/Components/SidebarMenu/SidebarMenu.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import {
   HomeFilled,
-  AppstoreFilled,
-  PieChartFilled,
+  CloseOutlined,
   AccountBookFilled,
+  MenuFoldOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, Button } from "antd";
+import { Layout, Menu } from "antd";
 import "./SidebarMenu.css";
+
 const { Sider } = Layout;
 
 function getItem(label, key, icon, children) {
@@ -18,33 +19,7 @@ function getItem(label, key, icon, children) {
   };
 }
 
-const menu1 = [
-  getItem("Dashboard", "1", <HomeFilled />),
-  getItem("Rate Limiting Policies", "sub1", null, [
-    getItem("Gateways", "3", <AppstoreFilled />),
-    getItem("API Categories", "4", <PieChartFilled />),
-    getItem("Key Managers", "5", <AccountBookFilled />),
-  ]),
-  getItem("Gateways", "6", <AppstoreFilled />),
-  getItem("API Categories", "7", <PieChartFilled />),
-  getItem("Key Managers", "8", <AccountBookFilled />),
-  getItem("Tasks", "sub2", null, [
-    getItem("User Creation", "9", <AppstoreFilled />),
-    getItem("Application Creation", "10", <PieChartFilled />),
-    getItem("Subscription Creation", "11", <AccountBookFilled />),
-    getItem("Subscription Update", "12", <AccountBookFilled />),
-    getItem("Application Registration", "13", <AccountBookFilled />),
-    getItem("API State Change", "14", <AccountBookFilled />),
-    getItem("Api Product State Change", "15", <AccountBookFilled />),
-  ]),
-  getItem("Settings", "sub3", null, [
-    getItem("Application", "17", <AppstoreFilled />),
-    getItem("Scope Arrangements", "18", <PieChartFilled />),
-    getItem("Advanced", "19", <AccountBookFilled />),
-  ]),
-];
-
-const menu2 = [
+const appMenu = [
   getItem("Overview", "overview", <HomeFilled />),
   getItem("Subscriptions", "subscription", <HomeFilled />),
   getItem("Try Out", "tryout", <HomeFilled />),
@@ -52,67 +27,69 @@ const menu2 = [
   getItem("Documentation", "documentation", <HomeFilled />),
   getItem("SDKs", "sdks", <HomeFilled />),
 ];
-const menu3 = [
-  getItem("Overview", "overview", <HomeFilled />),
-  getItem("Production Keys", "productionkeys", <HomeFilled />),
-  getItem("OAuth2 Tokens", "productionOauth2tokens", <HomeFilled />),
-  getItem("API Keys", "productionapikeys", <HomeFilled />),
+
+
+const apiMenu = [
+  
+  {
+    key: "overview",
+    icon: <HomeFilled />,
+    label: "Overview",
+    children: [
+      getItem("Subscriptions", "subscription", <AccountBookFilled />),
+      getItem("Sandbox Keys", "sandboxkeys", <HomeFilled />),
+      getItem("Oauth2 Tokens", "sandboxOauth2tokens", <HomeFilled />),
+      getItem("API Keys", "sandboxapikeys", <HomeFilled />),
+      getItem("Subscription", "subscription", <HomeFilled />),
+    ]
+  },
+  {
+    key: "subscription",
+    icon: <HomeFilled />,
+    label: "Subscriptions",
+    children: [
+      getItem("Subscriptions", "subscription", <AccountBookFilled />),
+      getItem("Sandbox Keys", "sandboxkeys", <HomeFilled />),
+      getItem("Oauth2 Tokens", "sandboxOauth2tokens", <HomeFilled />),
+      getItem("API Keys", "sandboxapikeys", <HomeFilled />),
+      getItem("Subscription", "subscription", <HomeFilled />),
+    ]
+  },
+   getItem("Subscriptions", "subscription", <AccountBookFilled />),
   getItem("Sandbox Keys", "sandboxkeys", <HomeFilled />),
   getItem("Oauth2 Tokens", "sandboxOauth2tokens", <HomeFilled />),
   getItem("API Keys", "sandboxapikeys", <HomeFilled />),
   getItem("Subscription", "subscription", <HomeFilled />),
 ];
-const SidebarMenu = ({
-  ChangeComponent,
-  selectedComponent,
-  handleChangeMenu,
-  changeMenu,
-}) => {
-  const [menu, setMenu] = useState(menu2);
+const SidebarMenu = ({ ChangeComponent, selectedComponent }) => {
+  const [menu, setMenu] = useState(apiMenu);
+  const [icon, setIcon] = useState(<MenuFoldOutlined />);
+
   const onClick = (e) => {
     ChangeComponent(e.key);
   };
+
+  const toggleMenu = () => {
+    if (icon.type === MenuFoldOutlined) {
+      setMenu(appMenu);
+      setIcon(<CloseOutlined />);
+    } else {
+      setMenu(apiMenu);
+      setIcon(<MenuFoldOutlined />);
+    }
+  };
+
   return (
     <Sider breakpoint="lg" collapsedWidth="80">
-      <div
-        style={{
-          padding: "1rem 0",
-          textAlign: "center",
-          color: "white",
-          fontSize: "2rem",
-        }}
+      <div className="logo"
       >
         Logo
       </div>
       <div>
-        <div className="button-div">
-          <Button
-            size="small"
-            className={changeMenu === "api" ? "api-btn active" : "api-btn"}
-            onClick={(e) => {
-              e.preventDefault();
-              handleChangeMenu("api");
-              ChangeComponent("overview");
-              setMenu(menu2);
-            }}
-          >
-            Api's
-          </Button>
-          <Button
-            size="small"
-            className={
-              changeMenu === "application" ? "api-btn active" : "api-btn"
-            }
-            onClick={(e) => {
-              e.preventDefault();
-              handleChangeMenu("application");
-              ChangeComponent("");
-              setMenu(menu3);
-            }}
-          >
-            {" "}
-            Applications
-          </Button>
+        <div className="icon-div">
+          <span className="icon-span icon" onClick={toggleMenu}>
+            {icon}
+          </span>
         </div>
       </div>
       <Menu
