908562157c837fd7401bc4cedfc51ffb7a53649b
hassanabbasi2213 <hassan.ababsi@propsure.com.pk>
2023-04-09 16:11:23 +0500

    fix header

--- src/Components/MenuHeader/MenuHeader.jsx
+++ src/Components/MenuHeader/MenuHeader.jsx
@@ -1,60 +1,65 @@
-import React from 'react'
-import {
-  SlackOutlined,
-  CaretDownOutlined
-} from '@ant-design/icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode,faFileArrowDown,faPrint,faFileLines,faList } from '@fortawesome/free-solid-svg-icons'
-import { Select } from 'antd';
-import './MenuHeader.css'
-const MenuHeader = ({ handleView, view }) => {
-  return (
-    <div className="Menu-Header-Container">
-      <div className="Menu-Header-Inner">
-        <FontAwesomeIcon icon={faCode} className="API-icon"/>
-        <div className='total-api'>
-          <h3 className='h3-medium'>APIs</h3>
-          <span className='totalapi-span'>Total:32</span>
-        </div>
-      </div>
+import React, {useState} from 'react';
+import {SlackOutlined, CaretDownOutlined} from '@ant-design/icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCode, faFileArrowDown, faPrint, faFileLines, faList} from '@fortawesome/free-solid-svg-icons';
+import './MenuHeader.css';
+import {Button, Modal} from 'antd';
 
-      <div className="Menu-Header-Inner">
-        {view === "ListView" && <>
-          <FontAwesomeIcon icon={faFileArrowDown} className="ListViewIcons"/>
-          <FontAwesomeIcon icon={faPrint} className="ListViewIcons"/>
-          <FontAwesomeIcon icon={faFileLines} className="ListViewIcons"/>
-        <div>
-          <Select
-          suffixIcon={<CaretDownOutlined className="ant-select-suffix downarrow-select"/>}
-            defaultValue="All"
-            style={{
-              width: 134,
-            }}
-            className="main-header"
-            options={[
-              {
-                value: 'All',
-                label: 'All',
-              },
-              {
-                value: 'lucy',
-                label: 'Lucy',
-              },
-              {
-                value: 'Yiminghe',
-                label: 'yiminghe',
-              },
-            ]}
-          />
-        </div>
-        </>
-        }
-        {/* <UnorderedListOutlined className={view !== "ListView" ? "ViewChangeIcon viewChangeInactive" : "ViewChangeIcon"} onClick={(e) => { e.preventDefault(); handleView("ListView") }} /> */}
-        <FontAwesomeIcon icon={faList} className={view === "ListView" ? "ViewChangeIcon viewChangeactive" : "ViewChangeIcon"} onClick={(e) => { e.preventDefault(); handleView("ListView") }}/>
-        <SlackOutlined className={view === "GridView" ? "ViewChangeIcon viewChangeactive" : "ViewChangeIcon"} onClick={(e) => { e.preventDefault(); handleView("GridView") }} />
-      </div>
-    </div>
-  )
-}
+const MenuHeader = ({handleView, view}) => {
+    const [open, setOpen] = useState(false);
 
-export default MenuHeader
+    return (
+        <div className="Menu-Header-Container">
+            <div className="Menu-Header-Inner">
+                <div className="total-api">
+                    <h3 className="h3-medium">APIs</h3>
+                    <span className="totalapi-span">Total APIs:32</span>
+                </div>
+
+            </div>
+
+            <div className="Menu-Header-Inner">
+                <Button style={{backgroundColor:'#16375A',color:"white"}} onClick={() => setOpen(true)}>
+                    Create APIs
+                    <CaretDownOutlined />
+                </Button>
+                <FontAwesomeIcon
+
+                    icon={faList}
+                    className={
+                        view === 'ListView'
+                            ? 'ViewChangeIcon viewChangeactive'
+                            : 'ViewChangeIcon'
+                    }
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleView('ListView');
+                    }}
+                />
+                <SlackOutlined
+                    className={
+                        view === 'GridView'
+                            ? 'ViewChangeIcon viewChangeactive'
+                            : 'ViewChangeIcon'
+                    }
+                    onClick={(e) => {
+                        e.preventDefault();
+                        handleView('GridView');
+                    }}
+                />
+
+                <Modal
+                    title="Create APIs"
+                    centered
+                    visible={open}
+                    onCancel={() => setOpen(false)}
+                    footer={null}
+                >
+                    <p>Modal content goes here.</p>
+                </Modal>
+            </div>
+        </div>
+    );
+};
+
+export default MenuHeader;
